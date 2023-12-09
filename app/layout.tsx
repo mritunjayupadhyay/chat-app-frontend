8274d93dde8e0a3b017bd5af556350b7a542e788
Mritunjay <mupadhyay00@gmail.com>
2023-12-10 03:26:35 +0700

    🔧 chore(layout.tsx): add SocketProvider to wrap children components to provide socket context 📦 feat(SocketContext.tsx): create SocketContext component to manage socket instance and provide it through context 🔧 chore(package.json): add socket.io-client dependency to package.json for socket functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/context/AuthContext'
+import { SocketProvider } from '@/context/SocketContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,7 +21,9 @@ export default function RootLayout({
    
       <body className={inter.className}>
       <AuthProvider>
+        <SocketProvider>
         {children}
+        </SocketProvider>
         </AuthProvider>
       </body>
      

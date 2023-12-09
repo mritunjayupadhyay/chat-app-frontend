2ececf9dc456263242baa0a8f0aee6b57667b29a
Mritunjay <mupadhyay00@gmail.com>
2023-12-09 18:47:21 +0700

    🎨 style(globals.css): update font import to include Archivo font for better typography
    🎨 style(globals.css): update global styles for truncate-1 class to improve text truncation
    
    🎨 style(globals.css): add keyframes for bounce animation to create a bouncing effect
    
    🎨 style(globals.css): add animation classes for elements to apply bounce animation
    
    ✨ feat(page.tsx): refactor LoginPage component to use functional component syntax and useState hook for managing input data
    
    ✨ feat(page.tsx): add Input and Button components to LoginPage for better UI and user interaction
    
    ✨ feat(page.tsx): implement handleDataChange function to update state when input data changes in LoginPage
    
    ✨ feat(page.tsx): implement handleLogin function to handle the login process in LoginPage
    
    ✨ feat(page.tsx): update JSX structure and add CSS classes for styling in LoginPage
    
    ✨ feat(page.tsx): add RegisterPage component to handle user registration
    
    ✨ feat(page.tsx): refactor RegisterPage component to use functional component syntax and useState hook for managing input data
    
    ✨ feat(page.tsx): add Input and Button components to RegisterPage for better UI and user interaction
    
    ✨ feat(page.tsx): implement handleDataChange function to update state when input data changes in RegisterPage
    
    ✨ feat(page.tsx): implement handleRegister function to handle the registration process in RegisterPage
    
    ✨ feat(button.tsx): add Button component for reusable button UI with customizable styles
    
    ✨ feat(input.tsx): add Input component for reusable input UI with customizable styles
    
    📦 chore(package.json): add cntl package as a dependency for better CSS-in-JS styling
    
    🔧 chore(tailwind.config.ts): add custom color palette for primary, secondary, dark, danger, and success colors

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,68 @@
 'use client'
+import React, { useState } from "react";
+import { useAuth } from "@/context/AuthContext";
+import Input from "@/components/input";
+import Button from "@/components/button";
 
-import React from 'react';
-import { useAuth } from '@/context/AuthContext';
-
-const LoginPage = () => {
-    const { logout } = useAuth();
-
-    const handleClick = async () => {
-        console.log('login', logout);
-          // Accessing the login function from the AuthContext
-          await logout();
-    }
+const Login = () => {
+    // State to manage input data (username and password)
+    const [data, setData] = useState({
+      username: "",
+      password: "",
+    });
+  
+    // Accessing the login function from the AuthContext
+    const { login } = useAuth();
+  
+    // Function to update state when input data changes
+    const handleDataChange =
+      (name: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        setData({
+          ...data,
+          [name]: e.target.value,
+        });
+      };
+  
+    // Function to handle the login process
+    const handleLogin = async () => await login(data);
+  
     return (
-        <div>
-        <h1>LoginPage</h1>
-        <button onClick={handleClick}>login</button>
+      <div className="flex justify-center items-center flex-col h-screen w-screen">
+        <div className="max-w-5xl w-1/2 p-8 flex justify-center items-center gap-5 flex-col bg-dark shadow-md rounded-2xl my-16 border-secondary border-[1px]">
+          <h1 className="inline-flex text-white items-center text-2xl mb-4 flex-col">
+             Login
+          </h1>
+          {/* Input for entering the username */}
+          <Input
+            placeholder="Enter the username..."
+            value={data.username}
+            onChange={handleDataChange("username")}
+          />
+          {/* Input for entering the password */}
+          <Input
+            placeholder="Enter the password..."
+            type="password"
+            value={data.password}
+            onChange={handleDataChange("password")}
+          />
+          {/* Button to initiate the login process */}
+          <Button
+            disabled={Object.values(data).some((val) => !val)}
+            fullWidth
+            onClick={handleLogin}
+          >
+            Login
+          </Button>
+          {/* Link to the registration page */}
+          <small className="text-zinc-300">
+            Don&apos;t have an account?{" "}
+            <a className="text-primary hover:underline" href="/register">
+              Register
+            </a>
+          </small>
         </div>
+      </div>
     );
-}
-
-export default LoginPage;
+  };
+  
+  export default Login;
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,11 +1,76 @@
 'use client'
+import React, { useState } from "react";
+import { useAuth } from "@/context/AuthContext";
+import Input from "@/components/input";
+import Button from "@/components/button";
 
-const RegisterPage = () => {
+const Register = () => {
+    // State to manage user registration data
+    const [data, setData] = useState({
+      email: "",
+      username: "",
+      password: "",
+    });
+  
+    // Access the register function from the authentication context
+    const { register } = useAuth();
+  
+    // Handle data change for input fields
+    const handleDataChange =
+      (name: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+        // Update the corresponding field in the data state
+        setData({
+          ...data,
+          [name]: e.target.value,
+        });
+      };
+  
+    // Handle user registration
+    const handleRegister = async () => await register(data);
+  
     return (
-        <div>
-        <h1>Register Page</h1>
+      // Register form UI
+      <div className="flex justify-center items-center flex-col h-screen w-screen">
+        <div className="max-w-5xl w-1/2 p-8 flex justify-center items-center gap-5 flex-col bg-dark shadow-md rounded-2xl my-16 border-secondary border-[1px]">
+          <h1 className="inline-flex text-white items-center text-2xl mb-4 flex-col">
+             Register
+          </h1>
+          {/* Input fields for username, password, and email */}
+          <Input
+            placeholder="Enter the email..."
+            type="email"
+            value={data.email}
+            onChange={handleDataChange("email")}
+          />
+          <Input
+            placeholder="Enter the username..."
+            value={data.username}
+            onChange={handleDataChange("username")}
+          />
+          <Input
+            placeholder="Enter the password..."
+            type="password"
+            value={data.password}
+            onChange={handleDataChange("password")}
+          />
+          {/* Register button */}
+          <Button
+            fullWidth
+            disabled={Object.values(data).some((val) => !val)}
+            onClick={handleRegister}
+          >
+            Register
+          </Button>
+          {/* Login link */}
+          <small className="text-zinc-300">
+            Already have an account?{" "}
+            <a className="text-primary hover:underline" href="/login">
+              Login
+            </a>
+          </small>
         </div>
+      </div>
     );
-}
-
-export default RegisterPage;
+  };
+  
+  export default Register;

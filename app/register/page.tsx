f0a61351e67d3a62021051de94d85b2d16b7bf9b
Mritunjay <mupadhyay00@gmail.com>
2023-12-24 04:38:19 +0700

    🐛 fix(page.tsx): fix import statements to use single quotes instead of double quotes for consistency ✨ feat(page.tsx): add functionality to upload profile picture during registration and display it on the UI

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,78 +1,132 @@
 'use client'
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import Link from 'next/link'
-import { useAuth } from "@/context/AuthContext";
-import Input from "@/components/input";
-import {Button} from "@/components/button";
-import { PaperClipIcon } from "@heroicons/react/20/solid";
+import { useAuth } from '@/context/AuthContext'
+import Input from '@/components/input'
+import { Button } from '@/components/button'
+import { PaperClipIcon } from '@heroicons/react/20/solid'
+import { upload } from '@/api/upload.api'
 
 const Register = () => {
     // State to manage user registration data
     const [data, setData] = useState({
-      name: "",
-      username: "",
-      password: "",
-    });
-  
+        name: '',
+        username: '',
+        password: '',
+    })
+    const [profilePic, setProfilePic] = useState<string>('')
+
     // Access the register function from the authentication context
-    const { register } = useAuth();
-  
+    const { register } = useAuth()
+
     // Handle data change for input fields
     const handleDataChange =
-      (name: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
-        // Update the corresponding field in the data state
-        setData({
-          ...data,
-          [name]: e.target.value,
-        });
-      };
-  
-    // Handle user registration
-    const handleRegister = async () => await register(data);
-    const handleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+        (name: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
+            // Update the corresponding field in the data state
+            setData({
+                ...data,
+                [name]: e.target.value,
+            })
+        }
 
+    // Handle user registration
+    const handleRegister = async () => await register(data)
+    const handleFileSelect = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            const file = e.target.files[0]
+            const res = await upload({
+                name: file.name,
+                type: file.type,
+            })
+            const {
+                data: { presignedUrl, objectKey },
+                success,
+            } = res.data
+            if (success !== true) return ''
+            // To save images.
+            const uploadToR2Response = await fetch(presignedUrl, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': file.type,
+                },
+                body: file,
+            })
+            console.log('uploadToR2Response', uploadToR2Response)
+            const url = `${process.env.NEXT_PUBLIC_R2_BUCKET_DOMAIN}/${objectKey}`
+
+            setProfilePic(url)
+        }
     }
-  
+
     return (
-      // Register form UI
-      <div className="flex justify-center items-center flex-col h-screen w-screen">
-        <div className="max-w-5xl w-1/2 p-8 flex justify-center items-center gap-5 flex-col bg-dark shadow-md rounded-2xl my-16 border-secondary border-[1px]">
-          <h1 className="inline-flex text-white items-center text-2xl mb-4 flex-col">
-             Register
-          </h1>
-          {/* Input fields for username, password, and email */}
-          <Input
-            placeholder="Enter the your name..."
-            value={data.name}
-            onChange={handleDataChange("name")}
-          />
-          <Input
-            placeholder="Enter the username..."
-            value={data.username}
-            onChange={handleDataChange("username")}
-          />
-          <Input
-            placeholder="Enter the password..."
-            type="password"
-            value={data.password}
-            onChange={handleDataChange("password")}
-          />
-          {/* Register button */}
-          <Button
-            fullWidth
-            disabled={Object.values(data).some((val) => !val)}
-            onClick={handleRegister}
-          >
-            Register
-          </Button>
-          {/* Login link */}
-          <small className="text-zinc-300">
-            Already have an account?{" "}
-            <Link className="text-primary hover:underline" href="/login">Login</Link>
-          </small>
+        // Register form UI
+        <div className="flex justify-center items-center flex-col h-screen w-screen">
+            <div className="max-w-5xl w-1/2 p-8 flex justify-center items-center gap-5 flex-col bg-dark shadow-md rounded-2xl my-16 border-secondary border-[1px]">
+                <h1 className="inline-flex text-white items-center text-2xl mb-4 flex-col">
+                    Register
+                </h1>
+                {/* Input fields for username, password, and name, profile pic */}
+                {profilePic ? (
+                    <div className="w-full flex justify-center py-2">
+                        <img src={profilePic} alt="" className="w-24 rounded-full" />
+                    </div>
+                ) : null}
+                <div className="w-full">
+                    <input
+                        hidden
+                        id="attachments"
+                        type="file"
+                        value=""
+                        multiple
+                        max={5}
+                        onChange={handleFileSelect}
+                    />
+                    <label
+                        htmlFor="attachments"
+                        className="w-full flex justify-center p-2 sm:p-4 rounded-full bg-bgInput hover:bg-bgPrimary"
+                    >
+                        Add Your Profile Pic{' '}
+                        <PaperClipIcon className="ml-2 w-5 h-5 sm:w-6 sm:h-6" />
+                    </label>
+                </div>
+                <Input
+                    placeholder="Enter the your name..."
+                    value={data.name}
+                    onChange={handleDataChange('name')}
+                />
+                <Input
+                    placeholder="Enter the username..."
+                    value={data.username}
+                    onChange={handleDataChange('username')}
+                />
+                <Input
+                    placeholder="Enter the password..."
+                    type="password"
+                    value={data.password}
+                    onChange={handleDataChange('password')}
+                />
+
+                {/* Register button */}
+                <Button
+                    fullWidth
+                    disabled={Object.values(data).some((val) => !val)}
+                    onClick={handleRegister}
+                >
+                    Register
+                </Button>
+                {/* Login link */}
+                <small className="text-zinc-300">
+                    Already have an account?{' '}
+                    <Link
+                        className="text-primary hover:underline"
+                        href="/login"
+                    >
+                        Login
+                    </Link>
+                </small>
+            </div>
         </div>
-      </div>
-    );
-  };
-  
-  export default Register;
+    )
+}
+
+export default Register

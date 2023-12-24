fc56fd2054dedbf56ce47c6eb7d156000e37cecd
Mritunjay <mupadhyay00@gmail.com>
2023-12-25 04:32:56 +0700

    🔧 fix(page.tsx): remove unused imports and variables to improve code cleanliness 🔧 fix(page.tsx): remove unused classes object to improve code readability ✨ feat(page.tsx): add authentication logic to redirect users to login page if no token is found, and to chat page if token is found

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,47 @@
+'use client'
+import { useAuth } from '@/context/AuthContext';
+import cntl from 'cntl';
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react';
 
 export const runtime = 'edge';
+const classes = {
+  backdrop: cntl`
+
+  `,
+  container: 'container',
+  sun: 'sun',
+  water: 'water',
+  fishy: 'fishy',
+}
 export default function Home() {
+  const router = useRouter()
+  const { token, user } = useAuth()
+
+  const handleClick = () => {
+    router.replace('/login')
+  }
+
+  useEffect(() => {
+      if (!token) {
+        console.log("No token found", token, user)
+          router.replace('/login')
+      } else {
+        console.log("Token found", token, user)
+          router.replace('/chat')
+      }
+  }, [token])
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Chat App</p>
+    <main className="min-h-screen relative overflow-hidden">
+      <section className="backdrop">
+        <button className="heading" onClick={handleClick}>Log in to connect other fish</button>
+      </section>
+
+      <section className="absolute h-screen w-full text-center top-0">
+        <div className="water"></div>
+        <div className="fishy"></div>
+      </section>
     </main>
   )
 }

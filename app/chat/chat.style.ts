f341dc8dd1f54147429cafa4c019b8642888e6d0
Mritunjay <mupadhyay00@gmail.com>
2023-12-28 06:21:51 +0700

    🔥 refactor(chat.style.ts): remove unused CSS classes and improve code readability
    The following changes were made:
    - Removed the unused `participantsAvatar` and `messageWindow` CSS classes.
    - Removed the unused `count` parameter from the `messageWindow` CSS class.
    - Removed the unused `isMessageWindowOpen` parameter from the `chatListContainer` CSS class.

--- app/chat/chat.style.ts
+++ app/chat/chat.style.ts
@@ -1,25 +1,6 @@
 import cntl from "cntl";
 
 const classes = {
-    participantsAvatar: (i: number) => cntl`
-    w-9 h-9 border-[1px] border-white rounded-full absolute outline outline-4 outline-dark
-    ${
-        i === 0
-        ? "left-0 z-30"
-        : i === 1
-        ? "left-2 z-20"
-        : i === 2
-        ? "left-4 z-10"
-        : ""
-    }`,
-    messageWindow: (count: number) => cntl`
-    p-8 overflow-y-auto flex flex-col-reverse gap-6 w-full bg-bgPrimary
-    ${
-        count > 0
-                    ? "h-[calc(100vh-336px)]"
-                    : "h-[calc(100vh-176px)]"
-    }
-    `,
     chatListContainer: (isMessageWindowOpen: boolean) => cntl`
     bg-bgPrimary relative ring-white overflow-y-auto
     w-full sm:w-1/3

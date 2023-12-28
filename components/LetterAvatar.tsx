014ce33243cc3e09d729dea56e9f999bc21ac581
Mritunjay <mupadhyay00@gmail.com>
2023-12-29 05:50:25 +0700

    ✨ feat(LetterAvatar.tsx): add LetterAvatar component to generate letter avatars based on name
    The LetterAvatar component is added to generate letter avatars based on the provided name. It takes a single prop, `name`, which is a string representing the name of the user. The component calculates the initials from the name and generates a background color based on the name using a hash function. The generated initials and background color are then used to render a circular avatar with the initials displayed in uppercase.

---
+++ components/LetterAvatar.tsx
@@ -0,0 +1,52 @@
+import cntl from "cntl";
+
+type PropTypes = {
+    name: string;
+}
+
+const classes = {
+    avatar: (color: string) => cntl`
+    
+    bg-[${color}]
+    `
+}
+const LetterAvatar = ({ name }: PropTypes) => {
+    const getInitials = (name: string) => {
+        const firstName = name.split(" ")[0];
+        const lastName = name.split(" ")[1];
+        const firstLetter = firstName[0];
+        const lastLetter = lastName ? lastName[0] : firstName[1];
+        return `${firstLetter}${lastLetter}`;
+      }
+    
+      const generateBackground = (name: string) => {
+        let hash = 0;
+        let i;
+    
+        for (i = 0; i < name.length; i += 1) {
+          hash = name.charCodeAt(i) + ((hash << 5) - hash);
+        }
+    
+        let color = "#";
+    
+        for (i = 0; i < 3; i += 1) {
+          const value = (hash >> (i * 8)) & 0xff;
+          color += `00${value.toString(16)}`.slice(-2);
+        }
+    
+        return color;
+      }
+    
+      let initials = getInitials(name);
+      let color = generateBackground(name);
+      const customStyle = {
+        background: color,
+      };
+    return (
+        <div className="flex w-12 h-12 rounded-full justify-center items-center text-white" style={customStyle}>
+        <span className="uppercase"> {initials} </span>
+      </div>
+    )
+}
+
+export default LetterAvatar;

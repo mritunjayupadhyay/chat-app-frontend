fe3840cd874957331b3453fa11bb5b263ac1cacc
Mritunjay <mupadhyay00@gmail.com>
2023-12-29 03:33:53 +0700

    🔧 chore(next.config.js): add remotePatterns configuration to next.config.js to allow loading images from a remote server 🐛 fix(chat.utils.ts): remove default avatar URL for group chats to prevent displaying a placeholder image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'pub-416a760c77f044d98b4a957925a825d5.r2.dev',
+            port: '',
+            pathname: '/**',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
--- utils/chat.utils.ts
+++ utils/chat.utils.ts
@@ -23,7 +23,7 @@ export const getChatObjectMetadata = (
       // Return metadata specific to group chats.
       return {
         // Default avatar for group chats.
-        avatar: "https://via.placeholder.com/100x100.png",
+        avatar: "",
         title: chat.name, // Group name serves as the title.
         description: `${chat.participants.length} members in the chat`, // Description indicates the number of members.
         lastMessage: chat.lastMessage
@@ -38,7 +38,7 @@ export const getChatObjectMetadata = (
       );
       // Return metadata specific to individual chats.
       return {
-        avatar: participant?.avatar || "https://via.placeholder.com/100x100.png", // Participant's avatar URL.
+        avatar: participant?.avatar || "", // Participant's avatar URL.
         title: participant?.username, // Participant's username serves as the title.
         description: participant?.name, // Email address of the participant.
         lastMessage,

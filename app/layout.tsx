491de23074e81e42b9f4609d5a35b6fa202929ab
Mritunjay <mupadhyay00@gmail.com>
2024-01-04 02:58:43 +0700

    🎨 style(layout.tsx): add favicon.ico link tag to improve website branding and user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-   
+      <head>
+      <link rel="icon" href="./favicon.ico" sizes="any" />
+      </head>
       <body className={[inter.className, "bg-dark", "text-white"].join(" ")}>
       <AuthProvider>
         <SocketProvider>
